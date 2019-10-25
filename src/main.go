289fec3f4c123869e84ad77e8f0c5f51370f5953
OlivierBP <olivier7397@gmail.com>
2019-10-25 17:37:14 +0200

    add phrasetoentropy and unit testing

--- src/main.go
+++ src/main.go
@@ -1,5 +1,5 @@
 // Project: PA193_mnemonic_aller
-// Mainteners UCO: 408788 497391 497577
+// Maintainers UCO: 408788 497391 497577
 // Description: Main file to test the API with command line
 
 package main
@@ -14,7 +14,23 @@ func main() {
   fmt.Println("Testing...")
 
   mnemonic.EntropyToPhraseAndSeed()
-  mnemonic.PhraseToEntropyAndSeed()
+
+
+  // Entropy from phrase
+
+  // phrase := "legal winner thank year wave sausage worth useful legal winner thank yellow"
+  phrase := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+  wlfile := "wordlists/english.txt"
+
+  if entropy, e := mnemonic.PhraseToEntropyAndSeed(phrase, wlfile); e != nil {
+    fmt.Println(e)
+  } else {
+    fmt.Println("Entropy:", entropy)
+  }
+
+
+
+
   mnemonic.VerifyPhraseAndSeed()
 
 }
--- src/mnemonic/mnemonic.go
+++ src/mnemonic/mnemonic.go
@@ -6,6 +6,12 @@ package mnemonic
 
 import (
   "fmt"
+  "strings"
+  "errors"
+  "io/ioutil"
+  "encoding/binary"
+  "math/big"
+  "encoding/hex"
 )
 
 func EntropyToPhraseAndSeed() {
@@ -13,10 +19,98 @@ func EntropyToPhraseAndSeed() {
   fmt.Println("TODO entropy_to_mnemonic")
 }
 
-func PhraseToEntropyAndSeed() {
-  // TODO
-  fmt.Println("TODO entropy_to_mnemonic")
+
+
+
+
+func PhraseToEntropyAndSeed(phrase string, wlfile string) (string, error) {
+
+  var wBytes [2]byte
+
+  // Use a big int for arbitrary-precision arithmetic of big numbers
+  bg := big.NewInt(0)
+  checksum := big.NewInt(0)
+  mask := big.NewInt(0)
+
+  wordsPhrase := strings.Fields(phrase)
+
+  nbWords := len(wordsPhrase)
+
+  // Nb words should be 12, 15, 18, 21 or 24
+  if nbWords!=12 && nbWords!=15 && nbWords!=18 && nbWords!=21 && nbWords!=24 {
+    return "", errors.New("Phrase Invalid")
+  }
+
+  // Read the wordlist file and extract words
+  content, e := ioutil.ReadFile(wlfile)
+  if e != nil {
+    return "", e
+  }
+  // Split the file into words, handle multi words on one line
+  wordsList := strings.Fields(string(content))
+
+  // Load the word list in a map
+  var wordsMap map[string]int = make(map[string]int)
+  for idx, word := range wordsList {
+    wordsMap[word] = idx
+  }
+
+  for _, wordP := range wordsPhrase {
+
+    // Get the index of the word in the wordsMap/wordlist
+    idx, found := wordsMap[wordP]
+    if found == false {
+      return "", errors.New("Phrase word not in wordlist: " + wordP)
+    }
+
+    // Concatenate the index to find back the binary vector
+    binary.BigEndian.PutUint16(wBytes[:], uint16(idx))
+
+    bg = bg.Mul(bg, big.NewInt(2048)) // Shift 11 bits
+    bg = bg.Or(bg, big.NewInt(0).SetBytes(wBytes[:]))
+  }
+
+  // The mask to get the checksum differs depending on the nb of words
+  switch nbWords {
+  case 12:
+    mask = big.NewInt(15)
+  case 15:
+    mask = big.NewInt(31)
+  case 18:
+    mask = big.NewInt(63)
+  case 21:
+    mask = big.NewInt(127)
+  case 24:
+    mask = big.NewInt(255)
+  }
+
+  // Get the checksum
+  checksum = checksum.And(bg, mask)
+  // Remove the checksum bits
+  bg.Div(bg, big.NewInt(0).Add(mask, big.NewInt(1)))
+
+  // The entropy is the rest of the bytes in the big int
+  // The left bits are filled with 0
+  entropy := bg.Bytes()
+  offset := nbWords/12 - len(bg.Bytes())
+  if offset > 0 {
+    entropy := make([]byte, nbWords/12)
+    copy(entropy[offset:], bg.Bytes())
+  }
+
+  // If the entropy is full 0, need to create the string manually
+  // The []byte become [] by default
+  var entropyStr string
+  if string(entropy) == "" {
+    entropyStr = string(strings.Repeat("0", 8*nbWords/3))
+  } else {
+    entropyStr = hex.EncodeToString(entropy)
+  }
+
+  return entropyStr, nil
 }
+
+
 
 func VerifyPhraseAndSeed() {
   // TODO

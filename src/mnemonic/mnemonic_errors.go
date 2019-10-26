9bebbf80b0c6b278afb15f1bd5a5d3ed04275cb7
quapka <quapka@gmail.com>
2019-10-26 14:55:07 +0200

    Add library prefix to custom errors

--- src/mnemonic/mnemonic_errors.go
+++ src/mnemonic/mnemonic_errors.go
@@ -6,10 +6,10 @@ import (
 )
 
 // TODO give more descriptive error messages
-const errEntropyNotDivisibleBy32 = "The 'entropy' bit-length '%d' is not divisible by 32."
-const errEntropyIsNotHexadecimal = "The 'entropy' is not a hexadecimal string."
-const errEntropyIsEmpty = "The 'entropy' is empty."
-const errENTNotInRange = "The 'entropy' bit-length is not in the range %d-%d."
+const errEntropyNotDivisibleBy32 = "PA193 mnemonic: The 'entropy' bit-length '%d' is not divisible by 32."
+const errEntropyIsNotHexadecimal = "PA193 mnemonic: The 'entropy' is not a hexadecimal string."
+const errEntropyIsEmpty = "PA193 mnemonic: The 'entropy' is empty."
+const errENTNotInRange = "PA193 mnemonic: The 'entropy' bit-length is not in the range %d-%d."
 
 func newEntropyNotDivisibleBy32Error(length int) error {
 	// FIXME add check for too big value

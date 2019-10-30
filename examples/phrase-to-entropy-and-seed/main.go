0b3d0f0401aed08185a5ae422fe6b5ba0b2f67dc
quapka <quapka@gmail.com>
2019-10-30 16:05:07 +0100

    Add example usage for PhraseToEntropyAndSeed

---
+++ examples/phrase-to-entropy-and-seed/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/quapka/PA193_mnemonic_aller/pkg/mnemonic"
+	"log"
+)
+
+func main() {
+	// example values taken from https://github.com/trezor/python-mnemonic/blob/master/vectors.json
+	// set the phrase
+	phrase := "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+	// optional set the passphrase, can be empty ""
+	passphrase := "TREZOR"
+	// set the filepath to the wordlist that should be used
+	wordlistFilepath := "../../wordlists/english.txt"
+	// calculate the entropy and seed
+	entropy, seed, err := mnemonic.PhraseToEntropyAndSeed(phrase, passphrase, wordlistFilepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Entropy:\n%s\n", entropy)
+	fmt.Printf("Seed:\n%s\n", seed)
+}
